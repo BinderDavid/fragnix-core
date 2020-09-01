4ef3020a2567e99b85aeef64d53b59c5ab1f0883
BinderDavid <binderd@informatik.uni-tuebingen.de>
2020-09-01 18:42:22 +0200

    Add Fragnix.Core.Update

---
+++ src/Fragnix/Core/Update.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Fragnix.Core.Update where
+
+import Data.Aeson (ToJSON, FromJSON)
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+import Fragnix.Core.Slice (SliceID)
+
+type Update = [(SliceID, SliceID)]
+
+type UpdateID = Text
+
+data PersistedUpdate = PersistedUpdate
+  { updateID :: UpdateID
+  , updateDescription :: Text
+  , updateContent :: Update
+  }
+
+deriving instance Show PersistedUpdate
+deriving instance Eq PersistedUpdate
+deriving instance Generic PersistedUpdate
+instance ToJSON PersistedUpdate
+instance FromJSON PersistedUpdate
+
